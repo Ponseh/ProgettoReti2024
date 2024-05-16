7fcdd7c19cd7374d2b0a692a5a2a871c6f8d89bd
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2024-05-16 14:57:09 +0200

    client.py style improvements

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
-def send_get_request(server_address="127.0.0.1", server_port=8000, 
-                    resource_path="/index.html"):
+def send_get_request(server_address='127.0.0.1', server_port=8000, 
+                    resource_path='/index.html'):
     # Creazione socket + connessione server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_address, server_port))
@@ -13,7 +13,7 @@ def send_get_request(server_address="127.0.0.1", server_port=8000,
     client_socket.sendall(request.encode())
 
     # Ricezione risposta
-    response = b""
+    response = b''
     while True:
         part = client_socket.recv(1024)
         if not part:
@@ -24,7 +24,7 @@ def send_get_request(server_address="127.0.0.1", server_port=8000,
     client_socket.close()
 
     # Decodifica e stampa risposta
-    print(response.decode("utf-8"))
+    print(response.decode('utf-8'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     send_get_request()
