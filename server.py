14cd45518941b923e14c161ce0bf4a9ad3531d93
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2024-05-16 14:50:47 +0200

    improved style of server.py and fixed a warning that was only shown in python version 3.12.3

--- server.py
+++ server.py
@@ -17,9 +17,9 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         parameters = parse_qs(parsed_url.query)
 
         if file_name == os.sep:
-            file_name = '\index.html'
+            file_name = '\\index.html'
 
-        absolutePath = os.getcwd() + os.sep + "resources" + file_name
+        absolutePath = os.getcwd() + os.sep + 'resources' + file_name
 
         if os.path.isfile(absolutePath):
             self.send_response(200)
@@ -30,10 +30,16 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             with open(absolutePath, 'rb') as f:
                 self.wfile.write(f.read())
             
-            print(f"Eseguita GET da {self.client_address} su file \"{file_name}\", utilizzato mime type \"{mime_type}\" per header e inviato codice di risposta \"200\" (OK)\nSono stati passati questi parametri: {parameters}\n")
+            print(f"Eseguita GET da {self.client_address} su "
+                  f"file \"{file_name}\", utilizzato mime type \"{mime_type}\" "
+                  f"per header e inviato codice di risposta \"200\" (OK)\nSono "
+                  f"stati passati questi parametri: {parameters}\n")
         else:
             self.send_error(404)
-            print(f"Eseguita GET {self.client_address} su file \"{file_name}\" inesistente, inviato codice di risposta \"404\" (Not Found)\nSono stati passati questi parametri: {parameters}\n")
+            print(f"Eseguita GET {self.client_address} su "
+                  f"file \"{file_name}\" inesistente, inviato "
+                  f"codice di risposta \"404\" (Not Found)\nSono "
+                  f"stati passati questi parametri: {parameters}\n")
 
 
 
@@ -41,11 +47,12 @@ class MyThreadedTCPServer(socketserver.ThreadingTCPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-def run(server_class=MyThreadedTCPServer, handler_class=MyHTTPRequestHandler, address='127.0.0.1', port=8000):
+def run(server_class=MyThreadedTCPServer, handler_class=MyHTTPRequestHandler,
+        address='127.0.0.1', port=8000):
     server_address = (address, port)
     httpd = server_class(server_address, handler_class)
     print(f"Server funzionante su: http://{address}:{port} ...\n")
     httpd.serve_forever()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
